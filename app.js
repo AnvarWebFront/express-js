33b220b31aaa672806f989cd003e1d8d6e3827b5
anvar mamarajabov <anvar.dev1909@gmail.com>
2025-08-28 23:27:55 +0500

    Database & env

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+require('dotenv').config();
 const express = require('express');
-
+const mongoose = require('mongoose');
+const postModel = require('./models/post.model')
 const app = express();
 
 // what is POST, GET, PUT, DELETE
 //endpoint
 
 app.use(express.json());
-app.get('/', (req, res)=>{
-	res.status(200).send('Hello World from Express.js');
+app.get('/', async (req, res)=>{
+	try {
+		const allPost = await postModel.find();
+		res.status(200).json(allPost);
+	} catch (error) {
+		res.status(500).json(error)
+	}
+	
 })
 //post -GET, POST, PUT, DELETE
-app.post('/', (req, res)=>{
-	const {firstName, lastName} = req.body;
-	const message = `His full name is ${firstName} ${lastName}`;
-	res.send(message);
+app.post('/', async (req, res)=>{
+	try {
+		const {title, body} = req.body;
+	const newPost = await postModel.create({title, body})
+	res.status(201).json(newPost);
+	} catch (error) {
+		res.status(500).json(error)
+	}
 })
 
 app.delete('/:id', (req, res)=>{
@@ -26,8 +38,20 @@ app.put('/:id', (req, res)=>{
 
 	res.json({id, body})
 })
-const PORT = 8080
-app.listen(PORT, () => console.log(`Listening on port -http://localhost:${PORT}` ));
+
+
+const PORT = process.env.PORT || 8080;
+
+
+const bootstrap = async ()=>{
+	try {
+		await mongoose.connect(process.env.DB_URL).then(()=>console.log('DB connected'));
+		app.listen(PORT, () => console.log(`Listening on port -http://localhost:${PORT}` ));
+	} catch (error) {
+		console.log(`Cannot connect to DB -${error}` );
+	}
+}
+bootstrap();
 
 // domain port endpoint
 // http://localhost:8080/posts
